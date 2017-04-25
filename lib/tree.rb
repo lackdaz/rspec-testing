fbcc2d476896fde52fa0bc9f104a71da153042c7
seth loh <haloandwings@gmail.com>
2017-04-26 07:27:52 +0800

    completed exercise

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,13 @@
 class Tree
+  attr_reader :type
+  attr_accessor :height, :age
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height += 0.1*water_amount
   end
 end
